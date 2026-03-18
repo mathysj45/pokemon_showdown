e4a6840232afbb4585775a8045b256b82013b453
Mathilde <marco.mathilde@orange.fr>
2026-03-18 12:10:03 +0100

    kk

--- src/main/java/com/example/pokemon_showdown/Database/ConnectionMySQL.java
+++ src/main/java/com/example/pokemon_showdown/Database/ConnectionMySQL.java
@@ -7,9 +7,14 @@ import java.sql.SQLException;
 public class ConnectionMySQL {
     private static final String URL = "jdbc:mysql://localhost:3306/showdown";
     private static final String USER = "root";
-    private static final String PASS = "demopma";
+    private static final String PASS = "";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found.", e);
+        }
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
